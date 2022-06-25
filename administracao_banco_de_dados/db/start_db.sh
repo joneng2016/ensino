1905bd44ff29b6a08dee0ebb03dfe887a76d62ef
Jonathan Samara <jonathan_samara@hotmail.com>
2022-06-25 15:54:27 -0300

    mudando detalhe para salvar o volume

--- administracao_banco_de_dados/db/start_db.sh
+++ administracao_banco_de_dados/db/start_db.sh
@@ -1,9 +1,9 @@
 echo 'Criando arquivo importante'
-cp -r /home/www/ensino/administracao_banco_de_dados/sample/ /home/www/ensino/administracao_banco_de_dados/db/db
+# cp -r /home/www/ensino/administracao_banco_de_dados/sample/ /home/www/ensino/administracao_banco_de_dados/db/db
 
 echo 'buildando imagem conforme dockerfile'
-docker build -t classdb .;
+# docker build -t classdb .;
 
 
 echo 'start do processo'
-docker run  -p 3305:3306 -v /home/www/ensino/administracao_banco_de_dados/db/db:/db  -e MYSQL_ROOT_PASSWORD=root  classdb;
+docker run -p 3305:3306 -v /home/www/ensino/administracao_banco_de_dados/db/db:/etc/mysql/conf.d -e MYSQL_ROOT_PASSWORD=root classdb;
