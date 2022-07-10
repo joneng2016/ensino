2391ff983779c105f6f6ae128c09324566285763
Jonathan Samara <jonathan_samara@hotmail.com>
2022-07-09 21:39:25 -0300

    falta resolver o bug da coluna do meio e preparar a aula

---
+++ minicursos/jogodavelha/data_of_game.py
@@ -0,0 +1,14 @@
+class DataOfGame:
+    
+    def __init__(self):
+
+        self.preto = (0, 0, 0)
+        self.branco = (255, 255, 255)
+        self.vermelho = (255, 0, 0)
+        self.verde = (0, 255, 0)
+        self.azul = (0, 0, 255)
+        self.cinza = (150, 150, 150)
+        self.click_last_status = 0
+        self.click_on_of = 0
+        self.click_position_x = -1
+        self.click_position_y = -1
