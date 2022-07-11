1d8c59699cabe9b9f055d6a492798b483c95504c
Jonathan Samara <jonathan_samara@hotmail.com>
2022-07-11 20:17:38 -0300

    criando a estrutura de academia

--- minicursos/laravel/academia/app/Http/Controllers/Home/HomeController.php
+++ minicursos/laravel/academia/app/Http/Controllers/Home/HomeController.php
@@ -8,6 +8,6 @@ use Illuminate\Http\Request;
 class HomeController extends Controller
 {
     public function index() {
-        return 'hello';
+        return view('home');
     }
 }
---
+++ minicursos/laravel/academia/resources/views/home.blade.php
@@ -0,0 +1,3 @@
+<html>
+    <h3>Hello</html>
+</html>
