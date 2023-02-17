500d452d95009998801d72ac8ff6fca76bc8e4d9
Jonathan Morris Samara <jonathan_samara@hotmail.com>
2023-02-17 20:09:03 -0300

    Create pratica2.sql

---
+++ banco de dados 1/pratica2.sql
@@ -0,0 +1,17 @@
+/*
+1 Tendo em vista a prática anterior, acesse o Xampp e verifique
+se o MySQL está UP
+
+2 Na mesma prática que a última aula - acesse o banco de dados via 
+linha de comando. Lembresse que o arquivo está em:
+cd C:\xampp\mysql\bin\
+
+Dentro deste diretório, acesse o banco - fazendo 
+.\mysql -u root
+3 Etapas 1 e 2 finalizdas, faça o seguinte: Crie uma base de dados
+chamada db_01. Acesse essa base de dados, crie quatro tabelas 
+dentro dessa base de dados - tabelas A, B, C e D. A tabela A deve
+ter uma coluna chamada nome com a tipagem varchar, a tabela B deve 
+ter uma coluna chamada idade e deve ser de tipagem integer. As tabelas 
+C e D devem ter duas colunas, uma  chamada nome com a tipagem varchar e outras com nome hoje de tipagem datetime
+*/
