cdd1c40ce01ba86a52c97662125353cb2b1e46e9
Jonathan Morris Samara <jonathan_samara@hotmail.com>
2023-02-17 20:10:57 -0300

    Create prática3.sql

---
+++ banco de dados 1/prática3.sql
@@ -0,0 +1,12 @@
+Abra o Windows Explore e acesse a pasta C:\
+Dentro de C:\ crie uma pasta, pode ser chamar meussql
+Abra o VS Code
+Acesse essa pasta usando o VSCode
+Crie um arquivo novo, poder ser meusql.sql
+Dentro desse arquivo fala o seguinte comando SELECT "OLA MUNDO" AS "MEU PRIMEIRO PRINT";
+Pegue o caminho absoluto desse arquivo
+Acesso o banco de dados via linha de comando e digite o seguinte comando
+source [Caminho desse arquivo] por exemplo: source C:\meussql\meusql.sql (no comando source não se coloca ;)
+-------------------------------------
+As tabelas 
+C e D devem ter duas colunas, uma  chamada nome com a tipagem varchar e outras com nome hoje de tipagem datetime
