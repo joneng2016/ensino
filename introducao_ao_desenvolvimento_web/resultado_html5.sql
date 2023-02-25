f37566c40aaf89452ce752ab803ad297a7e8aefe
Jonathan Morris Samara <jonathan_samara@hotmail.com>
2023-02-24 22:39:42 -0300

    Create resultado_html5.sql

---
+++ introducao_ao_desenvolvimento_web/resultado_html5.sql
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+    <head>
+        <title>Meu segundo HTML</title>
+        <meta charset="utf-8">
+
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+ <!--      
+    -->
+    </head>
+    <body>
+
+        <div style="margin-top:5px;background-color: blue;text-align: left; border: 1px solid; border-radius: 4px;">
+            Almoço
+        </div>
+        <div style="margin-top:5px;background-color: green; text-align: right;  border: 1px solid; border-radius: 4px;">
+            Caçada
+        </div>
+        <div style="margin-top:5px;background-color: yellow; text-align: center;  border: 1px solid; border-radius: 4px;">
+            Alacatrão
+        </div>
+        <div style="margin-top:5px;background-color: pink; text-align: center;  border: 1px solid; border-radius: 4px;">
+            Dicionário
+        </div>
+        <div style="margin-top:5px;background-color: white; text-align:right;  border: 1px solid; border-radius: 4px;">
+            Informação
+        </div>
+    </body>
+</html>
