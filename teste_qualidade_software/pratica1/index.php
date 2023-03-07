b717ff34cdefb80f229616a6e3d0b99a3728eb7d
Jonathan Morris Samara <jonathan_samara@hotmail.com>
2023-03-06 22:35:34 -0300

    Create index.php

---
+++ teste_qualidade_software/pratica1/index.php
@@ -0,0 +1,24 @@
+    <!DOCTYPE html>
+    <html lang="pt-br">
+        <head>
+            <meta charset="utf-8">
+            <meta name="viewport" content="width=device-width, initial-scale=1.0">
+            <title>Teste Caixa Preta 1</title>
+        </head>
+        <body>
+            <form method="POST" action="calculadados.php">
+                <input 
+                    type="number"
+                    step="0.01"
+                    placeholder="Entre com o valor do real"
+                    name="real_value"
+                >
+                <br>
+                <br>
+                <input 
+                    type="submit"
+                    value="Enviar Dado"
+                />
+            </form>
+        </body>
+    </html>
