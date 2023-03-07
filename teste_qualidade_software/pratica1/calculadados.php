1dc65d6a7aeb60aca0a53ee7b7e0d7fb3c788051
Jonathan Morris Samara <jonathan_samara@hotmail.com>
2023-03-06 22:36:03 -0300

    Create calculadados.php

---
+++ teste_qualidade_software/pratica1/calculadados.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Teste Caixa Preta 1</title>
+    </head>
+    <body>
+        <?php
+            $dolarValue = ((float) $_POST['real_value']) * 5.20;
+
+            $conn = new \PDO('mysql:host=localhost;dbname=dolar_real','root', '');
+            $sql = "INSERT INTO dolar (dolar_value) VALUES (?)";
+            $stmt= $conn->prepare($sql);
+            $stmt->execute([$dolarValue]);
+        
+        ?>
+        <h2>Requisição finalizada com sucesso</h2>
+        <h3>Foi escrito no banco de dados <?= $dolarValue ?></h3>
+    </body>
+</html>
