f4ed2cad34961586d8049f96c0feaa8f11b97912
Jonathan Morris Samara <jonathan_samara@hotmail.com>
2023-03-20 21:24:57 -0300

    Update index.php

--- teste_qualidade_software/pratica1/index.php
+++ teste_qualidade_software/pratica1/index.php
@@ -1,24 +1,37 @@
-    <!DOCTYPE html>
-    <html lang="pt-br">
-        <head>
-            <meta charset="utf-8">
-            <meta name="viewport" content="width=device-width, initial-scale=1.0">
-            <title>Teste Caixa Preta 1</title>
-        </head>
-        <body>
-            <form method="POST" action="calculadados.php">
-                <input 
-                    type="number"
-                    step="0.01"
-                    placeholder="Entre com o valor do real"
-                    name="real_value"
-                >
-                <br>
-                <br>
-                <input 
-                    type="submit"
-                    value="Enviar Dado"
-                />
-            </form>
-        </body>
-    </html>
+<!DOCTYPE html>
+<html lang="pt-br">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Teste Caixa Preta 1</title>
+        <style>
+            .text-input {
+                width: 300px;
+            }
+        </style>
+    </head>
+    <body>
+        <form method="POST" action="calculadados.php">
+            <input
+                class="text-input" 
+                type="text"
+                placeholder="Entre com o nome do usuário"
+                name="user_name"
+            >
+            <br>
+            <br>
+            <input 
+                type="number"
+                step="0.01"
+                placeholder="Entre com o valor do real"
+                name="real_value"
+            >
+            <br>
+            <br>
+            <input 
+                type="submit"
+                value="Enviar Dado"
+            />
+        </form>
+    </body>
+</html>
