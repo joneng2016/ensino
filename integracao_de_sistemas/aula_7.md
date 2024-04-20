321455387397276f9e2decdc46f230c6e817a9e4
Jonathan Morris Samara <jonathan_samara@hotmail.com>
2024-04-20 18:45:29 -0300

    integracao de sismteas

---
+++ integracao_de_sistemas/aula_7.md
@@ -0,0 +1,18 @@
+# Aula 7
+
+```
+docker pull rabbitmq:3-management
+docker run --rm -it -p 8080:15672 -p 5672:5672 rabbitmq:3-management
+```
+
+
+https://www.rabbitmq.com/tutorials/tutorial-one-javascript
+
+
+<img src="./figs/figuras/fig1.png" />
+
+<img src="./figs/figuras/fig2.png" />
+
+<img src="./figs/figuras/fig3.png" />
+
+<img src="./figs/figuras/fig4.png" />
---
+++ integracao_de_sistemas/praticas/pratica7/mensageria/reciving.js
@@ -0,0 +1,26 @@
+#!/usr/bin/env node
+
+const amqp = require('amqplib/callback_api');
+
+amqp.connect('amqp://localhost', function(error0, connection) {
+  if (error0) {
+    throw error0;
+  }
+  connection.createChannel(function(error1, channel) {
+    if (error1) {
+      throw error1;
+    }
+    const queue = 'hello';
+
+    channel.assertQueue(queue, {
+      durable: false
+    });
+
+    console.log(" [*] Waiting for messages in %s. To exit press CTRL+C", queue);
+    channel.consume(queue, function(msg) {
+        console.log(" [x] Received %s", msg.content.toString());
+    }, {
+        noAck: true
+    });
+  });
+});
